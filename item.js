ecffa0146d07e9ab1d71f7c79e9706523803b5ea
adrian santuario <santuario@ciencias.unam.mx>
2017-12-04 22:15:48 -0600

    item class

---
+++ item.js
@@ -0,0 +1,137 @@
+function Item(_id, _x, _y, _voluntaryNumber, _nameItem, _img, _imgIcons, _currentColor, _mobility) {
+  /*
+   *****************************************
+   *****************************************
+   * VARIABLES
+   *****************************************
+   *****************************************
+   */
+
+  this.id = _id;
+  this.x = _x;
+  this.y = _y;
+  this.voluntaryNumber = _voluntaryNumber;
+  this.nameItem = _nameItem;
+  this.img = _img;
+  this.imgIcons = _imgIcons;
+  this.currentColor = _currentColor;
+  this.mobility = _mobility;
+  this.voluntaryMaxNumber = 150;
+  this.overBox = false;
+  this.alphaValue = alphaValue = (this.voluntaryNumber / this.voluntaryMaxNumber) * 255.0;
+
+
+
+  /*
+   *****************************************
+   *****************************************
+   * LYFE CYCLE METHODS
+   *****************************************
+   *****************************************
+   */
+
+  this.display = function() {
+    tint(255, this.alphaValue);
+
+
+    var correctionX = (windowWidth / 2) - (this.img.width / 2);
+    var correctionY = (windowHeight / 2) - (this.img.height / 2);
+    image(this.img, correctionX, correctionY);
+
+
+    var c = this.img.get(mouseX + (correctionX * -1), mouseY + (correctionY * -1));
+
+    if (this.colorAlphaTest(c, 5)) {
+      this.displayInfo();
+    }
+
+
+  }
+
+
+  this.update = function() {
+
+  }
+
+  this.displayInfo = function() {
+    //textSize(32);
+    text("word", 10, 30);
+
+
+    var posX = 10;
+    var posY = 180;
+    noStroke();
+    fill(red(this.currentColor), green(this.currentColor), blue(this.currentColor), 255);
+
+    textAlign(LEFT, CENTER);
+
+    // Name
+    textSize(24);
+    var s = this.nameItem;
+    var sw = textWidth(s);
+    text(s, windowWidth - posX - 50 - sw, windowHeight - posY);
+
+    // Icons
+    image(this.imgIcons, windowWidth - posX - 50 - sw, windowHeight - posY + 20, this.imgIcons.width, this.imgIcons.height);
+
+
+    
+    // Rects Walk
+    rect(windowWidth - posX - 50 + 25 - sw, windowHeight - posY + 27, 150, 15);
+    // Rects Car
+    rect(windowWidth - posX - 50 + 25 - sw, windowHeight - posY + 47, 150, 15);
+    // Rects Bicy
+    rect(windowWidth - posX - 50 + 25 - sw, windowHeight - posY + 67, 150, 15);
+    // Rects Metro
+    rect(windowWidth - posX - 50 + 25 - sw, windowHeight - posY + 87, 150, 15);
+    // Rects Moto
+    rect(windowWidth - posX - 50 + 25 - sw, windowHeight - posY + 107, 150, 15);
+    
+    
+    // Scale
+    textAlign(RIGHT, CENTER);
+    fill(0, 255);
+    textSize(10);
+    var s = this.nameItem;
+    text("100%", windowWidth - posX - 50 + 25 - sw +150, windowHeight - posY + 127);
+    
+    
+    /*
+    // Cluster
+     textSize(18);
+     String s1 = "Cluster: " + str(centroidIndex);
+     text(s1, width-10, height-80);
+     */
+    // Category Count
+
+
+
+
+
+    stroke(red(this.currentColor), green(this.currentColor), blue(this.currentColor), 255);
+    //Line 
+    line(this.x, this.y, windowWidth - posX - 50 - sw, windowHeight - posY);
+
+  }
+
+
+
+  this.colorAlphaTest = function(_c1, _tol) {
+    if (red(_c1) + green(_c1) + blue(_c1) <= 1) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
+}
--- sketch.js
+++ sketch.js
@@ -8,20 +8,38 @@
  */
 
 
-/**/ //////////////////////////
-//////// VARIABLES
-////////////////////////////*/
+/*
+ *****************************************
+ *****************************************
+ * VARIABLES
+ *****************************************
+ *****************************************
+ */
 
-var img;
+var backgroundImage;
+var detailInfoImage;
+var items = [];
+var itemsCount = 1;
 
-/**/ ///////////////////////////
-///////////////////////////////
-//////// CYCLE LIFE
-///////////////////////////////
-//////////////////////////// */ 
+
+/*
+ *****************************************
+ *****************************************
+ * LYFE CYCLE METHODS
+ *****************************************
+ *****************************************
+ */
 
 function preload() {
-  img = loadImage("assets/images/CDMX_Template_0.png");
+
+  backgroundImage = loadImage("assets/images/CDMX_Template.png");
+  detailInfoImage = loadImage("assets/images/CDMX_Template_Icons.png");
+
+  for (var i = 0; i < itemsCount; i++) {
+    var nameImage = "assets/images/CDMX_Template_" + i + ".png";
+    window['img' + i] = loadImage(nameImage);
+  }
+
 }
 
 function windowResized() {
@@ -31,56 +49,95 @@ function windowResized() {
 
 function setup() {
   createCanvas(displayWidth, displayHeight);
-
+  initialize();
 }
 
 function draw() {
-  
+
   clear();
-  
-  tint(255, 150);
-  
-  //imageMode(CENTER)
-  var correctionX = (windowWidth/2)-(img.width/2);
-  var correctionY = (windowHeight/2)-(img.height/2); 
-  image(img, correctionX, correctionY);
-
-  var currentColor = color(134, 214, 226);
-
-
-
-  var c = img.get(mouseX+(correctionX*-1), mouseY+(correctionY*-1));
-  print(correctionX)
-
-
-  if (colorAlphaTest(c, 5)) {
-
-    textSize(32);
-    text("word", 10, 30);
-  } else {
-
-  }
-
-
-
-
-
-
-
-
-
-
+  drawBackground();
+  drawItems();
 
 }
 
 
+/*
+ *****************************************
+ *****************************************
+ * INITIALIZE METHODS
+ *****************************************
+ *****************************************
+ */
 
-function colorAlphaTest(_c1, _tol) {
+function initialize() {
+  initializeBackground();
+  initializeItems();
+}
 
 
-  if (red(_c1) + green(_c1) + blue(_c1) <= 1) {
-    return false;
-  } else {
-    return true;
+/**/ ///////////////////////////
+///////////////////////////////
+//////// BACKGROUND METHODS
+///////////////////////////////
+//////////////////////////// */ 
+
+function initializeBackground() {
+
+}
+
+function drawBackground() {
+  var correctionX = (windowWidth / 2) - (backgroundImage.width / 2);
+  var correctionY = (windowHeight / 2) - (backgroundImage.height / 2);
+  image(backgroundImage,correctionX, correctionY);
+}
+
+
+
+
+
+/*
+ *****************************************
+ *****************************************
+ * ITEMS METHODS
+ *****************************************
+ *****************************************
+ */
+
+
+function initializeItems() {
+  //itemsJSON.data.length
+  /*
+  canvasW = itemsJSON.data.length * 200 * sizeScale;
+  generalCanvasX = 0;
+  for (var i = 0; i < itemsJSON.data.length; i++) {
+    //var positionX = map(int(itemsJSON.data[i].year), -400, 2020, 0, 2020 * 3);
+    items.push(new Item(200 * i, windowHeight / 2, 100, 100, sizeScale, posX, posY,
+      itemsJSON.data[i].name,
+      itemsJSON.data[i].projectName,
+      itemsJSON.data[i].year,
+      itemsJSON.data[i].link,
+      itemsJSON.data[i].tag,
+      i));
+  }*/
+
+  for (var i = 0; i < itemsCount; i++) {
+    items.push(new Item(i,
+      100,
+      100,
+      110,
+      "Álvaro Obregón",
+      window['img' + i],
+      detailInfoImage,
+      color(167, 225, 234), [20, 20, 20, 20, 20]));
+
+  }
+
+}
+
+
+function drawItems() {
+  for (var i = 0; i < itemsCount; i++) {
+    items[i].update();
+    items[i].display();
   }
 }
